d06bb7d90671d756d2c3eb91a082db749934ae18
GiovaniFZ <giovanifinazzi@gmail.com>
2024-12-25 12:59:33 -0300

    conf: install react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
 export default function App() {
   return (
-    <h1>Hello world!</h1>
+    <RouterProvider router={router} />
   )
 }
